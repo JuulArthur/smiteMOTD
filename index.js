05a69200fbfbefa08fddd9630b35cca32c0eb628
Juul Rudihagen <juularthur92@gmail.com>
2017-01-22 23:01:39 +0100

    Add postmark to send mail

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
 const fetch = require('node-fetch');
-const app = express();
+const postmark = require('postmark');
+const apiKey = process.env.POSTMARK_API_KEY;
+const client = new postmark.Client(apiKey);
 
 fetch('https://motd.today/data.json').then(
     function (response) {
@@ -11,5 +12,12 @@ fetch('https://motd.today/data.json').then(
     console.log('Description: ' + body.motds[0].description);
     console.log('Number of player on each team: ' + body.motds[0].teamSize);
     console.log('Game mode: ' + body.motds[0].gameMode);
+    const mailBody = 'Hello fellow god, here is the latest news!\n\n' + 'Name: ' + body.motds[0].name + '\n' + 'Description: ' + body.motds[0].description + '\n' + 'Number of player on each team: ' + body.motds[0].teamSize + '\n' + 'Game mode: ' + body.motds[0].gameMode;
+    client.sendEmail({
+        "From": "juul@rudihagen.me",
+        "To": "juularthur92@gmail.com",
+        "Subject": "Smite MOTD",
+        "TextBody": mailBody
+    });
 });
 
